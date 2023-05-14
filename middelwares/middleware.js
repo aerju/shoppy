538d22a1dca50b2e574d4f93264702e469d2047f
Arjun <arj.kv42@gmail.com>
2023-05-14 13:44:59 +0530

    Block middleware added

--- middelwares/middleware.js
+++ middelwares/middleware.js
@@ -1,25 +1,33 @@
+const userHelpers = require("../helpers/user-helpers");
 
 module.exports = {
- checkAdminLoggedIn : (req, res, next) => {
+  checkAdminLoggedIn: (req, res, next) => {
     if (req.session.adminLoggedIn) {
-        next();
+      next();
     } else {
-        res.redirect('/admin');
+      res.redirect("/admin");
     }
-}, sessionHandle : (req,res,next)=>{
-   
-    if(req.session.userLoggedIn){
-      res.redirect('/')
-    }else{
-      next();
-    }
-  
   },
-  checkUserLoggedIn:(req,res,next)=>{
+  sessionHandle: (req, res, next) => {
     if (req.session.userLoggedIn) {
+      res.redirect("/");
+    } else {
       next();
-  } else {
-      res.redirect('/login');
-  }
-  }
-}
+    }
+  },
+  checkUserLoggedIn: async (req, res, next) => {
+    if (req.session.userLoggedIn) {
+      let user = await userHelpers.getUserDta(req.session.user._id);
+      if (user.userstatus) {
+        next();
+      } else {
+        {
+          req.session.userLoggedIn = false;
+          res.redirect("/login");
+        }
+      }
+    } else {
+      res.redirect("/login");
+    }
+  },
+};
